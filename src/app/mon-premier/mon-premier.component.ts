0f5bd0d2830d8fa5d5df8a3c62cdc8de55f77ede
matthieu <matthieubeaussart8@gmail.com>
2020-04-30 14:59:04 +0200

    ajout de variables

--- src/app/mon-premier/mon-premier.component.ts
+++ src/app/mon-premier/mon-premier.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MonPremierComponent implements OnInit {
 
+  premierName = 'LeFast';
+  premierStatus = 'rapide';
+
   constructor() { }
 
   ngOnInit(): void {
